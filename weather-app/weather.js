ea435e87fcc19dd6e81101a18c31e6b4c2f50115
leejm1 <wnsaud6016@naver.com>
2019-06-02 19:58:16 +0900

    icon

--- weather-app/weather.js
+++ weather-app/weather.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo';
+import { Ionicons } from "@expo/vector-icons";
 
 export default class Weather extends Component{
     render(){
         return(
         <LinearGradient colors={["#00C6FB","#005BEA"]} style={styles.container}>
             <View style={styles.upper}>
-                <Text>Icon Here</Text>
+                <Ionicons color="white" name="ios-rainy" size={144}></Ionicons>
                 <Text style={styles.temp}>38 °C</Text>
             </View>
             <View style={styles.lower}>
