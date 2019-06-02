ce9dc4e472ab7f00ee00b3bedfc20accf4277d7e
leejm1 <wnsaud6016@naver.com>
2019-06-02 20:11:14 +0900

    error 처리

--- weather-app/App.js
+++ weather-app/App.js
@@ -4,7 +4,8 @@ import Weather from "./weather";
 
 export default class App extends Component {
   state = {
-    isLoaded: false
+    isLoaded: false,
+    error:null
   };
 
   componentDidMount(){
@@ -15,19 +16,22 @@ export default class App extends Component {
         });
       },
       error =>{
-        console.log(error);
+        this.setState({
+          error:error
+        });
       }
     )
   }
 
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true}/>
         { isLoaded ? <Weather /> : 
                           <View style={styles.loading}>
                             <Text style={styles.loadingText}>Getting the weather... wait Plese</Text>
+                            {error ? <Text style={styles.errorText}>{error}</Text> : null}
                           </View>
         }
       </View>
@@ -39,6 +43,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  errorText:{
+    color: "red",
+    backgroundColor: "transparent"
   },
   loading: {
     flex: 1,
